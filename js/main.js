eeb13b48437e46a7ee4617b69c397417abd458ce
superssol <86730207+superssol@users.noreply.github.com>
2021-07-30 20:38:51 +0900

    Added file

--- js/main.js
+++ js/main.js
@@ -12,3 +12,33 @@ searchinputel.addEventListener("blur",function(){
   searchel.classList.remove("focused");
   searchinputel.setAttribute("placeholder","");
 });
+
+const badgeel = document.querySelector("header .badges");
+
+window.addEventListener("scroll", _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+//배지 숨기기
+
+    gsap.to(badgeel, .6, {
+      opacity: 0
+      ,display: "none"
+    });
+  }
+  else {
+//배지 보이기
+    gsap.to(badgeel, .6, {
+      opacity: 1
+      ,display: "block"
+    });
+  }
+},300));
+// _.throttle(함수,시간)
+
+const fadeels = document.querySelectorAll(".visual .fade-in");
+fadeels.forEach(function (a, index) {
+  gsap.to(a, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
