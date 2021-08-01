a75dd2a86ccb71eba7db5838cbf91bf542c7f39d
superssol <86730207+superssol@users.noreply.github.com>
2021-08-01 22:09:55 +0900

    upload 8/1

--- js/main.js
+++ js/main.js
@@ -42,3 +42,10 @@ fadeels.forEach(function (a, index) {
     opacity: 1
   });
 });
+
+// new Swiper(선택자, 옵션)
+new Swiper(".notice-line .swiper-container", {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
