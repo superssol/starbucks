68f627449673c5437715726a69ceeb1c5f8445a1
superssol <86730207+superssol@users.noreply.github.com>
2021-08-02 16:48:54 +0900

    d-1

--- js/main.js
+++ js/main.js
@@ -82,3 +82,24 @@ promotionToggleBtn.addEventListener("click",function () {
     promotionEl.classList.remove("hide");
   }
 })
+
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selelctor, delay, size) { 
+  gsap.to(selelctor, //선택자
+     random(1.5, 2.5), //애니메이션 동작시간
+  { //옵션
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  }
+  );
+}
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", .5, 15);
+floatingObject(".floating3", 1.5, 20);
