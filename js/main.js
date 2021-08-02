8f4aba23312f22a667491a497b243e950befb64a
superssol <86730207+superssol@users.noreply.github.com>
2021-08-02 13:32:31 +0900

    tooooo hard

--- js/main.js
+++ js/main.js
@@ -49,3 +49,36 @@ new Swiper(".notice-line .swiper-container", {
   autoplay: true,
   loop: true
 });
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 5000
+  // },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지번호 선택자
+    clickable: true  
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next"
+  }
+
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promoption");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click",function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    //숨기처리
+    promotionEl.classList.add("hide");
+  }else {
+    //보임처리
+    promotionEl.classList.remove("hide");
+  }
+})
